bfa44ab9588b307dd8c136e2a8d4eb28710d9f77
wsauvan <wsauvan@webqam.fr>
2023-06-19 23:55:13 +0200

    feat(splade): installed splade

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,42 +1,18 @@
-import { createApp } from 'vue/dist/vue.esm-bundler';
-import { pascalToKebab } from './utils/String';
+import "./bootstrap";
+import "../css/app.css";
+import "@protonemedia/laravel-splade/dist/style.css";
 
-import ExampleComponent from './components/ExampleComponent.vue';
+import { createApp } from "vue/dist/vue.esm-bundler.js";
+import { renderSpladeApp, SpladePlugin } from "@protonemedia/laravel-splade";
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
+const el = document.getElementById("app");
 
-// const files = require.context('./', true, /\.vue$/i);
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
-
-// Instantiate Vue root components
-[ExampleComponent].forEach((c) => {
-  const componentName = c.__name || c.name;
-
-  if (process.env.NODE_ENV === 'development' && !componentName) {
-    throw new Error('Component name is undefined.');
-  }
-
-  [...document.getElementsByTagName(pascalToKebab(componentName))].forEach(
-    (element) => {
-      const wrapper = document.createElement('div');
-      element.parentNode.insertBefore(wrapper, element);
-      wrapper.appendChild(element);
-
-      const app = createApp(wrapper);
-      app.component(componentName, c);
-      app.mount(wrapper);
-    }
-  );
-});
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+createApp({
+    render: renderSpladeApp({ el })
+})
+    .use(SpladePlugin, {
+        "max_keep_alive": 10,
+        "transform_anchors": false,
+        "progress_bar": true
+    })
+    .mount(el);
